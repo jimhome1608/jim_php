01c538d194f89cbc71d2dd5b8379f177b3daaf66
Jim Home <jimhome1608@gamil.com>
2015-05-17 20:59:56 +1000

    test

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,9 @@
 <html>
         <head>
                 <title>CodeIgniter Tutorial</title>
+                <p><a href="<?php echo base_url('/index.php/Home'); ?>">Home</a></p>
+                <p><a href="<?php echo base_url('/index.php/About'); ?>">About</a></p>
+                <p><a href="<?php echo base_url('/index.php/news'); ?>">News</a></p>
         </head>
         <body>
 
