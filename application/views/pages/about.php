5ca2fa90355b8d1e05ae01855c98da4675100b94
Jim Home <jimhome1608@gamil.com>
2015-05-20 21:42:54 +1000

    made into a web app

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,1 @@
-<img src="../images/Owl.jpg" />
+<img src="../images/Owl.jpg" />"
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,30 +1,15 @@
 <html>
-        <head>
-                <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-                <title>Jims PHP Site</title>
-                <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo base_url('/index.php/Home'); ?>">Jim Clark</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">        
-        <li><a href="<?php echo base_url('/index.php/news'); ?>">News</a></li>
-        <li><a href="<?php echo base_url('/index.php/About'); ?>">About</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-        </head>
-        <body>
-
-                <h2><?php echo $title ?></h2>
+   <head>
+     <meta name="viewport" content="width=device-width">
+     <meta name="mobile-web-app-capable" content="yes"> 
+     <link rel="icon" sizes="128x128" href="../images/smile-icon.png">
+     <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+     <title>Jims PHP Site</title>                
+  	<ol class="breadcrumb">
+  		<li><a href="<?php echo base_url('/index.php/Home'); ?>">Home</a></li>
+  		<li><a href="<?php echo base_url('/index.php/news'); ?>">News</a></li>
+  		<li><a href="<?php echo base_url('/index.php/About'); ?>">About</a></li>    		
+	</ol>
+   </head>
+<body>
+<!--  <h2><?php echo $title ?></h2>  -->
