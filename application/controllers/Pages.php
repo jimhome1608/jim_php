00f0ec82d1784f176b20e91bb705a367bef6d84b
Jim Home <jimhome1608@gamil.com>
2015-05-26 06:44:19 +1000

    better javascript

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -20,9 +20,11 @@ public function view($page = 'home')
         $data['title'] = ucfirst($page);
        // if ($page == 'Home')
            $data['title'] = ''; //ucfirst($page); // Capitalize the first letter
-         
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/'.$page, $data);
-        $this->load->view('templates/footer', $data);
+        if ($page != 'script') 
+       		$this->load->view('templates/header', $data);
+         $this->load->view('pages/'.$page, $data);
+         if ($page != 'script')
+           $this->load->view('templates/footer', $data);
+       
 }
 }
--- application/views/pages/serialport.php
+++ application/views/pages/serialport.php
@@ -3,18 +3,19 @@
 <p></p>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script type="text/javascript">
-function btnClick (clicked_id) {
+function btnClick (sender_id) {
+	command_data = "{".concat(sender_id,"00FF00}");
     $.ajax({
       url:"script", //the page containing php script
       type: "POST", //request type
-      data: ({Command:clicked_id}),
+      data: ({Command:command_data}),
       success:function(result){
-        alert(clicked_id);
+        alert(result);
      }
    });
 }
 </script>
-<button type="button" onclick="btnClick(this.id)" id="{000FF00FF}">LED On</button>
-<button type="button" onclick="btnClick(this.id)" id="{000000000}">LED Off</button>
+<button type="button" onclick="btnClick(this.id)" id="000">LED 000</button>
+<button type="button" onclick="btnClick(this.id)" id="100">LED 100</button>
 </body>
 </html>
